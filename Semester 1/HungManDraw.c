f54a2c18f8c97b1b4aea0d4d6e6dd50c6faf37ee
MoshPe <fghghf98@gmail.com>
2020-03-23 20:56:22 +0200

    .

---
+++ Semester 1/HungManDraw.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("    O\n    |\n   /|\\\n  / | \\\n   / \\\n  /   \\\n");
+
+printf("    O\n");
+printf("    |\n");
+printf("   /|\\\n");
+printf("  / | \\\n");
+printf("    | \n");
+printf("   / \\\n");
+printf("  /   \\\n");
+
+return 0;
+}
