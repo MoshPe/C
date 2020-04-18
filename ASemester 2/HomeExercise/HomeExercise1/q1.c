612d5b2f893bc8e81d2058ef9e7fa160b1798e45
MoshPe <fghghf98@gmail.com>
2020-04-18 22:16:56 +0300

    .

--- ASemester 2/HomeExercise/HomeExercise1/q1.c
+++ ASemester 2/HomeExercise/HomeExercise1/q1.c
@@ -20,44 +20,52 @@ void main()
 Function name: calculate
 Input: getting the fraction struct and the quatity of the fractions
 Output: outputting the smallest fraction possible  
-Algoritem: first it counts the mone and the mechane together by the rules
+Algoritem: first it counts the mone and the mehane together by the rules
 			to one fraction, then the fucntion finds the highest number that
 			divide them both
 */
 void calculate(fraction* s, int size)
 {
 	int i, count = 0;
-	long int t,mechaneX,moneX;
-	long int mechane = s[0].b;
+	long int t,mehaneX,moneX;
+	long int mehane = s[0].b;
 	long int mone = 0;
 	for (i = 1; i < size; i++)
-		mechane = mechane * s[i].b;
+		mehane = mehane * s[i].b;
 	for (i = 0; i < size; i++)
-		mone = mone + s[i].a * (mechane / s[i].b);
+		mone = mone + s[i].a * (mehane / s[i].b);
 	for (i = 0; i < size; i++)
 		printf(" %d/%d %c", s[i].a, s[i].b, (i == size - 1) ? '=' : '+');
 		//for not losing the original number
 		moneX = mone;
-		mechaneX = mechane;
-		//calculates the highest number that divide both mone and mechane
+		mehaneX = mehane;
+		/*calculates the highest number that divide both mone and mehane
+		  and put it in moneX  */
 	do {
-		t = moneX % mechaneX;
-		if (2 * t > mechaneX)
-			t = mechaneX - t;
-		moneX = mechaneX;
-		mechaneX = t;
-	} while (moneX != 0 && mechaneX != 0);
+		t = moneX % mehaneX;
+		if (2 * t > mehaneX)
+			t = mehaneX - t;
+		moneX = mehaneX;
+		mehaneX = t;
+	} while (moneX != 0 && mehaneX != 0);
 	mone = mone / moneX;
-	mechane = mechane / moneX;
-	while (mone>mechane)
+	mehane = mehane / moneX;
+	while (mone>mehane)
 	{
-		mone = mone - mechane;
+		mone = mone - mehane;
 		count++;
 	}
 	if (count > 0)
 		printf("%ld and", count);
-	printf(" %ld/%ld", mone, mechane);
+	printf(" %ld/%ld", mone, mehane);
 }
+/*
+Function name: outputInfo
+Input: getting the fraction struct and the quatity of the fractions
+Output: outputting the smallest fraction possible  
+Algoritem: it first getting input all the fraction from the user and put them
+			into the structure array and send the array to the calculate function
+*/
 void outputInfo(fraction* s, int size)
 {
 	int i, index;
@@ -66,6 +74,7 @@ void outputInfo(fraction* s, int size)
 	{
 		printf("%d (mone mehane): ", i + 1);
 		scanf("%d%d", &s[i].a, &s[i].b);
+		//checking if the fraction is correct and valid
 		if (s[i].a != 0 && s[i].b == 0)
 		{
 			printf("invalid denominator,please try again!\n");
